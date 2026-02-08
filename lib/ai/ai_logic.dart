ce2d4f98121b6c12be9cdc615f823151c2d9c97c
Radhikavala60 <160853802+Radhikavala60@users.noreply.github.com>
2026-02-08 13:16:47 +0530

    Delete lib/ai/ai_logic.dart

--- lib/ai/ai_logic.dart
+++
@@ -1,9 +0,0 @@
-String getAISuggestion(String priority, int hours) {
-  if (priority == "High" && hours < 2) {
-    return "AI Suggestion: More time should be given to this topic";
-  } else if (priority == "Medium") {
-    return "AI Suggestion: Revise this topic regularly.";
-  } else {
-    return "AI Suggestion: This topic is currently in balance.";
-  }
-}
